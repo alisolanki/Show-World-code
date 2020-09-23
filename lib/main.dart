38095292b68fa7b4e948d924492fc3bfc2afe433
Adnan Solanki <adnansolanki@Adnans-iMac.local>
2020-09-23 17:32:33 +0530

    Oreintation Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 
 import './LoginOTP/pages/splash_page.dart';
 import './LoginOTP/stores/login_store.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.landscapeLeft]).then((_){
+      runApp(MyApp());
+    });
+}
 
 class MyApp extends StatelessWidget {
   @override
